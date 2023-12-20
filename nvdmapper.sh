6efea7d3a2480b428b2763e6fd445db07cdee657
Ethan <ethan@saenger5.com>
2023-12-20 13:59:51 -0600

    removed unnecessary '.html' from wget -E
    changed keyword function to overwrite keywords.txt rather than append

--- nvdmapper.sh
+++ nvdmapper.sh
@@ -9,11 +9,11 @@ echo scanning...
 mkdir $ip
 cd $ip/
 nmap $ip -p $port1-$port2 -sV -oX scan.xml
-grep 'open' scan.xml | grep 'service name' | cut -d= -f6-9 | cut -c 6- | cut -d\" -f4,6 >> keywords.txt
+grep 'open' scan.xml | grep 'service name' | cut -d= -f6-9 | cut -c 6- | cut -d\" -f4,6 > keywords.txt
 while read line ; do
 	name=$(echo "$line" | cut -d\" -f1)
 	url="https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="${name// /+}"&search_type=all&isCpeNameSearch=false"
-	wget -E .html $url
+	wget -E $url
 	echo $url >> links.txt
 done < keywords.txt
 for file in *; do cat $file | grep vuln-summary >> summary.txt; done
